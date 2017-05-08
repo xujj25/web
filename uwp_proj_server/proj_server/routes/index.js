619f7e319583cb22d0867ef6cb9be76f197d84e1
xujj25 <xujj25@foxmail.com>
2017-05-08 18:52:03 +0800

    add test news data

--- uwp_proj_server/proj_server/routes/index.js
+++ uwp_proj_server/proj_server/routes/index.js
@@ -13,16 +13,16 @@ router.get('/news', function(req, res, next) {
     err_msg: '',
     news: [
       {
-        _id: 12345678,
-        img_url: 'http://cn.bing.com',
-        title: 'hello',
-        src_site: 'http://cn.bing.com'
+        _id: '1_0_2870152_1171_1494230640',
+        img_url: 'http://a1.huanqiu.cn/images/73fa315aeaa61eafe9990df120afa5c8.jpg',
+        title: '许魏洲迷彩风街拍曝光  眼神清澈帅气养眼',
+        src_site: '环球网'
       },
       {
-        _id: 987654321,
-        img_url: 'http://cn.bing.com',
-        title: 'world',
-        src_site: 'http://cn.bing.com'
+        _id: '1_0_2870151_127_1494230220',
+        img_url: 'http://a1.huanqiu.cn/images/276ff3e5b4352644c2dd208625054484.jpg',
+        title: '德国汉诺威发现二战遗留炸弹 紧急疏散5万居民',
+        src_site: '东方IC'
       }
     ]
   };
